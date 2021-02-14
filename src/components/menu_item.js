dd44c14cd3a17d96bdf800cbdbfc78eb209b1093
Alex <ajhollid@gmail.com>
2021-02-14 14:01:27 -0800

    add menu items

--- src/components/menu_item.js
+++ src/components/menu_item.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React from "react"
+import styled from "styled-components"
+import Img from "gatsby-image"
 
-const MenuItem = ({item}) => {
-    return(
-        <h1>{item}</h1>
-    )
+const MenuImage = styled(Img)`
+  max-width: 70px;
+  max-height: 70px;
+  border-radius: 50%;
+  margin: 0 1rem 0 1rem;
+  flex: 1;
+`
+
+const MenuText = styled.p`
+  font-size: 1rem;
+`
+
+const MenuItemContainer = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+const MenuItem = ({ fluid, name }) => {
+  return (
+    <li>
+      <MenuItemContainer>
+        <MenuImage fluid={fluid} />
+        <MenuText>{name}</MenuText>
+      </MenuItemContainer>
+    </li>
+  )
 }
+
+export default MenuItem
--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,35 +1,44 @@
-import React from "react";
-import MenuItem from '../components/menu_item'
-import Img from "gatsby-image"
+import React from "react"
+import MenuItem from "../components/menu_item"
 
 const Menu = ({ data }) => {
-    const pictures = data.allImageSharp.nodes;
-    console.log(pictures)
-    const items = data.allMenuItemsCsv.nodes
-    return (
-        <>
-        <ol>
-            {items.map((item) => {
-                return (
-                    <li>
-
-                        {item.name}
-                    </li>)
-            })}
-        </ol>
-
-        <ol>
-            {pictures.map((picture)=> {
-                return (
-                    <li><Img fluid={picture.fluid}/></li>
-                )
-            })}
-        </ol>
-        </>
+  const pictures = data.allFile.nodes.map(node => {
+    const obj = {}
+    const name = node.childImageSharp.fluid.originalName.replace(
+      /\.[^/.]+$/,
+      ""
     )
+    const fluid = node.childImageSharp.fluid
+    obj[name] = fluid
+    return obj
+  })
+
+  const picLookup = pictures.reduce((acum, cur) => {
+    const key = Object.keys(cur)[0]
+    acum[key] = cur[key]
+    return acum
+  }, {})
+
+  const items = data.allMenuItemsCsv.nodes
+
+  const menuItems = items.map(item => {
+    return (
+      <MenuItem
+        key={item.code}
+        fluid={
+          picLookup[item.code]
+            ? picLookup[item.code]
+            : data.file.childImageSharp.fluid
+        }
+        name={item.name}
+      />
+    )
+  })
+
+  return <ol>{menuItems}</ol>
 }
 
-export default Menu;
+export default Menu
 export const pageQuery = graphql`
   query {
     site {
@@ -38,18 +47,33 @@ export const pageQuery = graphql`
       }
     }
     allMenuItemsCsv {
-        nodes {
-          name
+      nodes {
+        code
+        name
+      }
+    }
+    allFile(
+      filter: {
+        sourceInstanceName: { eq: "assets" }
+        relativeDirectory: { eq: "menu" }
+      }
+    ) {
+      nodes {
+        childImageSharp {
+          fluid(maxWidth: 200, maxHeight: 200) {
+            originalName
+            ...GatsbyImageSharpFluid
+          }
         }
-      }  
-    allImageSharp {
-        nodes {
-            fluid(maxWidth: 200, maxHeight: 200) {
-                originalName
-                ...GatsbyImageSharpFluid
-              }
+      }
+    }
+    file(extension: { eq: "png" }, name: { eq: "place_holder" }) {
+      childImageSharp {
+        fluid(maxWidth: 200, maxHeight: 200) {
+          originalName
+          ...GatsbyImageSharpFluid
         }
+      }
     }
   }
 `
-
