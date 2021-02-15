9d29515ba67fe003ad1c8fcc7228072b3140df0b
Alex <ajhollid@gmail.com>
2021-02-15 10:40:51 -0800

    add external links

--- src/components/Navbar/NavbarLinks.js
+++ src/components/Navbar/NavbarLinks.js
@@ -38,14 +38,62 @@ const NavItem = styled(Link)`
   }
 `
 
+const ExternalNavItem = styled.a`
+  text-decoration: none;
+  color: #111;
+  display: inline-block;
+  white-space: nowrap;
+  margin: 0 1vw;
+  transition: all 200ms ease-in;
+  position: relative;
+
+  :after {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    width: 0%;
+    content: ".";
+    color: transparent;
+    background: goldenrod;
+    height: 1px;
+    transition: all 0.4s ease-in;
+  }
+
+  :hover {
+    color: goldenrod;
+    ::after {
+      width: 100%;
+    }
+  }
+
+  @media (max-width: 768px) {
+    padding: 20px 0;
+    font-size: 1.5rem;
+    z-index: 6;
+  }
+`
+
 const NavbarNavItems = () => {
   return (
     <>
       <NavItem to="/">Home</NavItem>
       <NavItem to="/menu">Menu</NavItem>
-      <NavItem to="/order">Online Order</NavItem>
-      <NavItem to="/delivery">Delivery</NavItem>
-      <NavItem to="/gift">E-Gift Cards</NavItem>
+      <ExternalNavItem
+        href="https://ccs-chinese-restaurant-online-order.square.site/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Online Order
+      </ExternalNavItem>
+      <ExternalNavItem to="/delivery">Delivery</ExternalNavItem>
+      <ExternalNavItem
+        href="https://squareup.com/gift/E7DQNVQS4CR2V/order"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        E-Gift Cards
+      </ExternalNavItem>
     </>
   )
 }
