c5eb9dc95fd8e89543bd7d013012f68c74d8a125
Alex <ajhollid@gmail.com>
2021-02-17 08:59:51 -0800

    style carousel, replace media query

--- src/components/carousel/carousel.js
+++ src/components/carousel/carousel.js
@@ -4,15 +4,22 @@ import { Link } from "gatsby"
 
 import styled from "styled-components"
 import "react-responsive-carousel/lib/styles/carousel.min.css"
+import "./override.css"
 import { Carousel } from "react-responsive-carousel"
+import { useMediaQuery } from 'react-responsive'
+
 
 const CarouselImage = styled(Img)`
   max-height: 500px;
+  margin: 0 0.2rem 0 0.2rem;
 `
 
+
 const PostCarousel = ({ posts }) => {
+const isTabletOrMobile = useMediaQuery({ query: '(max-width: 42rem)' })
+
   return (
-    <Carousel infiniteLoop centerMode centerSlidePercentage={50}>
+    <Carousel infiniteLoop centerMode={!isTabletOrMobile} centerSlidePercentage={50}>
       {posts.map(post => {
         return (
           <Link to={post.fields.slug} itemProp="url">
