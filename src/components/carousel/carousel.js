808c5901c3dbe323466efdd80c7ceea0a5ccb153
Alex <ajhollid@gmail.com>
2021-02-16 22:42:14 -0800

    use react responsive carousel

--- src/components/carousel/carousel.js
+++ src/components/carousel/carousel.js
@@ -1,27 +1,21 @@
 import React from "react"
 import Img from "gatsby-image"
-import styled from "styled-components"
-
-const CarouselContainer = styled.div`
-  display: flex;
-`
+import styled from "styled-components";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 
 const CarouselImage = styled(Img)`
   max-height: 500px;
-  flex: 1;
 `
 
-
-const Carousel = ({ posts }) => {
-  console.log(posts[0])
+const PostCarousel = ({ posts }) => {
   return (
-    <CarouselContainer>
-      <CarouselImage fluid={posts[0].frontmatter.featuredImage.childImageSharp.fluid}></CarouselImage>
-      <CarouselImage fluid={posts[0].frontmatter.featuredImage.childImageSharp.fluid}></CarouselImage>
-      <CarouselImage fluid={posts[0].frontmatter.featuredImage.childImageSharp.fluid}></CarouselImage>
-
-    </CarouselContainer>
+    <Carousel infiniteLoop centerMode centerSlidePercentage={50}>
+      <CarouselImage fluid={posts[0].frontmatter.featuredImage.childImageSharp.fluid}/>
+      <CarouselImage fluid={posts[0].frontmatter.featuredImage.childImageSharp.fluid}/>
+      <CarouselImage fluid={posts[0].frontmatter.featuredImage.childImageSharp.fluid}/>
+    </Carousel>
   )
 }
 
-export default Carousel
+export default PostCarousel
