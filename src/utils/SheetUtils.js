9bebdefe062dbf0e722d589ccc056882b2e620ec
Alex <ajhollid@gmail.com>
2021-02-20 10:38:34 -0800

    Implement event opening

--- src/utils/SheetUtils.js
+++ src/utils/SheetUtils.js
@@ -1,8 +1,10 @@
 const FilterRsvps = (rsvps, date) => {
+  console.log(rsvps)
+  console.log(date)
   const result = []
   const map = new Map()
   for (const item of rsvps) {
-    if (!map.has(item.email) && parseInt(item.orderDate, 10) === date) {
+    if (!map.has(item.email) && parseInt(item.orderDate, 10) === date.getTime()) {
       map.set(item.email, true)
       result.push({
         orderDate: item.orderDate,
