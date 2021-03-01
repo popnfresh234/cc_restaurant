275186e84b79886786a738b02b332d8781348ae1
Alex <ajhollid@gmail.com>
2021-03-01 09:11:02 -0800

    Add conditional link for ordering to bowen

--- src/components/DeliveryCard/DeliveryCard.js
+++ src/components/DeliveryCard/DeliveryCard.js
@@ -3,14 +3,14 @@ import Img from "gatsby-image"
 import styled from "styled-components"
 import { useStaticQuery, graphql, Link } from "gatsby"
 import DateUtils from "../../utils/DateUtils"
-import SheetUtils from"../../utils/SheetUtils"
+import SheetUtils from "../../utils/SheetUtils"
 
 const CardContainer = styled.div`
   display: flex;
   flex-direction: column;
   flex: 1;
   @media (max-width: 42rem) {
-  border: 1px solid black;
+    border: 1px solid black;
     padding: 0.8rem;
     width: 50vw;
     margin: 0 0 2rem 0;
@@ -26,8 +26,14 @@ const SmallText = styled.div`
   font-size: 0.9rem;
 `
 
+const OrderText = styled.div`
+  margin: 0.5rem 0 0 0;
+  font-size: 1.2rem;
+  font-weight: bold;
+  text-align: center;
+`
 
-const DeliveryCard = ({rsvps,date,index}) => {
+const DeliveryCard = ({ rsvps, date, index }) => {
   const currentRsvps = SheetUtils.FilterRsvps(rsvps, date)
   const data = useStaticQuery(graphql`
     query {
@@ -55,7 +61,7 @@ const DeliveryCard = ({rsvps,date,index}) => {
   `)
   return (
     <CardContainer>
-      <Link to="../rsvp" state={{date: date, rsvps: rsvps}}>
+      <Link to="../rsvp" state={{ date: date, rsvps: rsvps }}>
         <CardContents>
           <span>Current RSVPs: {currentRsvps.length}</span>
           {currentRsvps.length > 4 ? (
@@ -69,6 +75,12 @@ const DeliveryCard = ({rsvps,date,index}) => {
           </SmallText>
         </CardContents>
       </Link>
+      {currentRsvps.length > 4 && (
+        <a href="https://ccs-chinese-restaurant-online-order.square.site/">
+          {" "}
+          <OrderText>Cick here to order!</OrderText>
+        </a>
+      )}
     </CardContainer>
   )
 }
